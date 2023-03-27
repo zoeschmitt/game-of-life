25e8b7d0ba773be7b10125b55ac458ddab2e8ad2
Zoe Schmitt <zoeschmitt@hotmail.com>
2023-03-26 19:26:09 -0500

    first com

---
+++ go.mod
@@ -0,0 +1,8 @@
+module life/cells
+
+go 1.20
+
+require (
+	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6 // indirect
+	github.com/go-gl/glfw v0.0.0-20221017161538-93cebf72946b // indirect
+)
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/go-gl/gl/v4.1-core/gl"
+	"github.com/go-gl/glfw/v3.2/glfw"
+	"log"
+	"runtime"
+)
+
+const (
+	width  = 500
+	height = 500
+)
+
+func main() {
+	// Ensures we will always execute in the same operating system thread,
+	// which is important for GLFW which must always be called from the same thread it was initialized on.
+	runtime.LockOSThread()
+
+	window := initGlfw()
+	defer glfw.Terminate()
+
+	for !window.ShouldClose() {
+		// TODO
+	}
+}
+
+// initGlfw initializes glfw and returns a Window to use.
+func initGlfw() *glfw.Window {
+	if err := glfw.Init(); err != nil {
+		panic(err)
+	}
+
+	glfw.WindowHint(glfw.Resizable, glfw.False)
+	glfw.WindowHint(glfw.ContextVersionMajor, 4) // OR 2
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+
+	window, err := glfw.CreateWindow(width, height, "Conway's Game of Life", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+	window.MakeContextCurrent()
+
+	return window
+}
