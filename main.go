4ce84a0cdecd25f4d769fa10d92710f3e0e3237d
Zoe Schmitt <zoeschmitt@hotmail.com>
2023-03-26 19:32:48 -0500

    feat: opening window

--- main.go
+++ main.go
@@ -20,8 +20,10 @@ func main() {
 	window := initGlfw()
 	defer glfw.Terminate()
 
+	program := initOpenGL()
+
 	for !window.ShouldClose() {
-		// TODO
+		draw(window, program)
 	}
 }
 
@@ -32,11 +34,12 @@ func initGlfw() *glfw.Window {
 	}
 
 	glfw.WindowHint(glfw.Resizable, glfw.False)
-	glfw.WindowHint(glfw.ContextVersionMajor, 4) // OR 2
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
 	glfw.WindowHint(glfw.ContextVersionMinor, 1)
 	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
 	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
 
+	// Binding the window to our current thread.
 	window, err := glfw.CreateWindow(width, height, "Conway's Game of Life", nil, nil)
 	if err != nil {
 		panic(err)
@@ -44,4 +47,31 @@ func initGlfw() *glfw.Window {
 	window.MakeContextCurrent()
 
 	return window
+}
+
+// initOpenGL initializes OpenGL and returns an intiialized program.
+func initOpenGL() uint32 {
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+	version := gl.GoStr(gl.GetString(gl.VERSION))
+	log.Println("OpenGL version", version)
+
+	// A program gives us a reference to store shaders, which can then be used for drawing.
+	prog := gl.CreateProgram()
+	gl.LinkProgram(prog)
+	return prog
+}
+
+func draw(window *glfw.Window, program uint32) {
+	// Remove anything from the window that was drawn last frame, giving us a clean slate.
+	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+	gl.UseProgram(program)
+
+	// Check if there were any mouse or keyboard events.
+	glfw.PollEvents()
+	// Buffer swapping is important because GLFW (like many graphics libraries) uses double buffering,
+	// meaning everything you draw is actually drawn to an invisible canvas, and only put onto the
+	// visible canvas when you’re ready - which in this case, is indicated by calling SwapBuffers
+	window.SwapBuffers()
 }
