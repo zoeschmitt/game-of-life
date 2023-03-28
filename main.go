deb2da2ef1b94e15c824f84917a9b6db75637d1e
Zoe Schmitt <zoeschmitt@hotmail.com>
2023-03-27 22:10:12 -0500

    feat: divided board and drawing squares

--- main.go
+++ main.go
@@ -1,16 +1,69 @@
 package main
 
 import (
+	"fmt"
 	"github.com/go-gl/gl/v4.1-core/gl"
 	"github.com/go-gl/glfw/v3.2/glfw"
 	"log"
 	"runtime"
+	"strings"
 )
 
 const (
-	width  = 500
-	height = 500
+	width   = 500
+	height  = 500
+	rows    = 10
+	columns = 10
+	// These are strings containing GLSL source code for two shaders, one for a vertex shader and another for a fragment shader.
+	// The only thing special about these strings is that they both end in a null-termination character, \x00 - a requirement for
+	// OpenGL to be able to compile them. Make note of the fragmentShaderSource, this is where we define the color of our shape
+	// in RGBA format using a vec4. You can change the value here, which is currently RGBA(1, 1, 1, 1) or white, to change the
+	// color of the triangle.
+	vertexShaderSource = `
+    #version 410
+    in vec3 vp;
+    void main() {
+        gl_Position = vec4(vp, 1.0);
+    }
+` + "\x00"
+	fragmentShaderSource = `
+    #version 410
+    out vec4 frag_colour;
+    void main() {
+        frag_colour = vec4(1, 1, 1, 1);
+    }
+` + "\x00"
 )
+
+// The slice contains 9 values, three for each vertex of a triangle.
+// The top line, 0, 0.5, 0, is the top vertex represented as X, Y, and Z coordinates,
+// the second line is the left vertex, and the third line is the right vertex.
+// Each of these pairs of three represents the X, Y, and Z coordinates of the vertex relative
+// to the center of the window, between -1 and 1.
+var (
+	triangle = []float32{
+		-0.5, 0.5, 0, // top
+		-0.5, -0.5, 0, // left
+		0.5, -0.5, 0, // right
+	}
+	square = []float32{
+		-0.5, 0.5, 0,
+		-0.5, -0.5, 0,
+		0.5, -0.5, 0,
+
+		-0.5, 0.5, 0,
+		0.5, 0.5, 0,
+		0.5, -0.5, 0,
+	}
+)
+
+type cell struct {
+	// A drawable is a square Vertex Array Object.
+	drawable uint32
+
+	x int
+	y int
+}
 
 func main() {
 	// Ensures we will always execute in the same operating system thread,
@@ -21,9 +74,10 @@ func main() {
 	defer glfw.Terminate()
 
 	program := initOpenGL()
+	cells := makeCells()
 
 	for !window.ShouldClose() {
-		draw(window, program)
+		draw(cells, window, program)
 	}
 }
 
@@ -57,16 +111,34 @@ func initOpenGL() uint32 {
 	version := gl.GoStr(gl.GetString(gl.VERSION))
 	log.Println("OpenGL version", version)
 
+	vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
+	if err != nil {
+		panic(err)
+	}
+	fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
+	if err != nil {
+		panic(err)
+	}
+
 	// A program gives us a reference to store shaders, which can then be used for drawing.
 	prog := gl.CreateProgram()
+	gl.AttachShader(prog, vertexShader)
+	gl.AttachShader(prog, fragmentShader)
 	gl.LinkProgram(prog)
 	return prog
 }
 
-func draw(window *glfw.Window, program uint32) {
+func draw(cells [][]*cell, window *glfw.Window, program uint32) {
 	// Remove anything from the window that was drawn last frame, giving us a clean slate.
 	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 	gl.UseProgram(program)
+
+	// Loop over each cell and have it draw itself.
+	for x := range cells {
+		for _, c := range cells[x] {
+			c.draw()
+		}
+	}
 
 	// Check if there were any mouse or keyboard events.
 	glfw.PollEvents()
@@ -74,4 +146,123 @@ func draw(window *glfw.Window, program uint32) {
 	// meaning everything you draw is actually drawn to an invisible canvas, and only put onto the
 	// visible canvas when you’re ready - which in this case, is indicated by calling SwapBuffers
 	window.SwapBuffers()
+}
+
+// makeVao initializes and returns a vertex array from the points provided.
+// vao = Vertex Array Object
+func makeVao(points []float32) uint32 {
+	// First we create a Vertex Buffer Object or vbo to bind our vao to, which is created by providing
+	// the size (4 x len(points)) and a pointer to the points (gl.Ptr(points)).
+	// You may be wondering why it’s 4 x len(points) - why not 6 or 3 or 1078?
+	// The reason is we are using float32 slices, and a 32-bit float has 4 bytes, so we are saying
+	// the size of the buffer, in bytes, is 4 times the number of points.
+	var vbo uint32
+	gl.GenBuffers(1, &vbo)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.BufferData(gl.ARRAY_BUFFER, 4*len(points), gl.Ptr(points), gl.STATIC_DRAW)
+
+	var vao uint32
+	gl.GenVertexArrays(1, &vao)
+	gl.BindVertexArray(vao)
+	gl.EnableVertexAttribArray(0)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 0, nil)
+
+	return vao
+}
+
+// a vertex shader manipulates the vertices to be drawn by OpenGL and generates the data passed to the fragment shader,
+// which then determines the color of each fragment (you can just consider a fragment to be a pixel) to be drawn to the screen.
+// The purpose of this function is to receive the shader source code as a string as well as its type,
+// and return a pointer to the resulting compiled shader.
+func compileShader(source string, shaderType uint32) (uint32, error) {
+	shader := gl.CreateShader(shaderType)
+
+	csources, free := gl.Strs(source)
+	gl.ShaderSource(shader, 1, csources, nil)
+	free()
+	gl.CompileShader(shader)
+
+	var status int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
+	}
+
+	return shader, nil
+}
+
+// makeCells creates and returns a 2D slice of pointers to cell structs.
+// The slice has a length of 'rows', and each row has a length of 'columns'.
+// Each cell in the slice is a new cell struct created using the newCell function.
+// Returns the 2D slice of cell pointers.
+func makeCells() [][]*cell {
+	cells := make([][]*cell, rows, rows)
+	for x := 0; x < rows; x++ {
+		for y := 0; y < columns; y++ {
+			c := newCell(x, y)
+			cells[x] = append(cells[x], c)
+		}
+	}
+
+	return cells
+}
+
+func newCell(x, y int) *cell {
+	// Create a copy of our square definition. This allows us to change its contents to customize
+	// the current cell’s position, without impacting any other cells that are also using the square slice.
+	points := make([]float32, len(square), len(square))
+	copy(points, square)
+
+	// Next we iterate over the points copy and act based on the current index. We use a modulo operation
+	// to determine if we’re at an X (i % 3 == 0) or Y (i % 3 == 1) coordinate of the shape (skipping Z since
+	// we’re operating in two dimensions) and determine the size (as a percentage of the entire game board)
+	// of the cell accordingly, as well as it’s position based on the X and Y coordinate of the cell on the game board.
+	for i := 0; i < len(points); i++ {
+		var position float32
+		var size float32
+		switch i % 3 {
+		case 0:
+			size = 1.0 / float32(columns)
+			position = float32(x) * size
+		case 1:
+			size = 1.0 / float32(rows)
+			position = float32(y) * size
+		default:
+			continue
+		}
+
+		// Modify the points which currently contain a combination of 0.5, 0 and -0.5 as we defined them in the
+		// square slice. If the point is less than zero, we set it equal to the position times 2 (because OpenGL
+		// coordinates have a range of 2, between -1 and 1), minus 1 to normalize to OpenGL coordinates.
+		// If the position is greater than or equal to zero, we do the same thing but add the size we calculated.
+		// The purpose of this is to set the scale of each cell so that it fills only its percentage of the game board.
+		// Since we have 10 rows and 10 columns, each cell will be given 10% of the width and 10% of the height of the game board.
+		if points[i] < 0 {
+			points[i] = (position * 2) - 1
+		} else {
+			points[i] = ((position + size) * 2) - 1
+		}
+	}
+
+	// After all the points have been scaled and positioned, we create a cell with the X and Y coordinate provided,
+	// and set the drawable field equal to a Vertex Array Object created from the points slice we just manipulated.
+	return &cell{
+		drawable: makeVao(points),
+
+		x: x,
+		y: y,
+	}
+}
+
+// Each cell needs to know how to draw itself.
+func (c *cell) draw() {
+	gl.BindVertexArray(c.drawable)
+	gl.DrawArrays(gl.TRIANGLES, 0, int32(len(square)/3))
 }
